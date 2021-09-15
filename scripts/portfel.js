ffb6f85a527b5f5fa716e44a53e73093dcab6471
kamil <kamil1006@o2.pl>
2021-09-15 16:45:45 +0200

    second commit

---
+++ scripts/portfel.js
@@ -0,0 +1,518 @@
+
+var p0_html_content = {
+    "w1": "<p>First project. Testing Excel VBA. After that games in Android appeared.</p>",
+    "w4": "<p>Own wersion of simple game in VBA in Excel using Forms.</p>",
+	"w5": "<p>Level: in Excel. Hard to say.</p>"
+	
+}
+
+var p0_href = "";
+var p0_href2 = "css/images2/vba1a.jpg";
+var p0_video = "";
+var p0_href3 = "";
+
+
+var p1_html_content = {
+    "w1": "<p>Project is about making microservice for saving and getting examples of code and sharing it.",
+    "w2": " It uses POST and GET requests with JSON objects,",
+	"w3": " forms are one of the way to enter and show data stored in the database.</p>",
+    "w4": "<p>Own secure version of a code-sharing platform.</p>",
+	"w5": "<p>Level: challenging</p>"
+	
+}
+var p1_href = "https://github.com/kamil1006/Code_Sharing_Platform";
+
+
+var p2_html_content = {
+    "w1": "<p>During the project implementation, you will learn the basics of SQL and work with the H2 database.",
+    "w2": "You will also learn about advanced Java features such as Collections. It uses console as interface.",
+	"w3": "</p>",
+    "w4": "<p>Program that manages a car-sharing service allowing companies to rent out their cars and find customers.</p>",
+	"w5": "<p>Level: challenging</p>"
+	
+}
+var p2_href = "https://github.com/kamil1006/Car_Sharing_koniec";
+
+
+var p3_html_content = {
+    "w1": "<p>First project. Own version of the popular Battleship Game . Uses console as interface.",
+    "w2": "",
+	"w3": "</p>",
+    "w4": "",
+	"w5": "<p>Level: challenging</p>"
+	}
+var p3_href = "https://github.com/kamil1006/Battleship";
+
+
+
+var p4_html_content = {
+    "w1": "<p>Multi-users web service for creating and solving quizzes",
+    "w2": "",
+	"w3": "</p>",
+    "w4": "<p>spring boot microservice. You will learn about REST API, an embedded database, security, and other technologies, like pagination</p>",
+	"w5": "<p>Level: challenging</p>"
+	}
+var p4_href = "https://github.com/kamil1006/Web_Quiz_Engine";
+
+
+
+var p5_html_content = {
+    "w1": "<p>Java SWING text editor allows to search for specific  text fragments in files and edit them.",
+    "w2": "",
+	"w3": "</p>",
+    "w4": "",
+	"w5": "<p>Level: challenging</p>"
+	}
+var p5_href = "https://github.com/kamil1006/Text_Editor";
+var p5_href2 = "css/images2/texteditor.png";
+
+
+var p6_html_content = {
+    "w1": "<p>You will get a firm grasp of working with JSON in Java. You will also practice ",
+    "w2": "parallelization and see how to implement processing multiple requests at the same time.",
+	"w3": "</p>",
+    "w4": "<p>JSon are stored in files and sent to database. Client-server application that allows the clients to store their data on the server in JSON format</p>",
+	"w5": "<p>Level: challenging</p>"
+	}
+var p6_href = "https://github.com/kamil1006/JSON_Database";
+
+
+
+
+var p7_html_content = {
+    "w1": "<p>This site :-)",
+    "w2": " but now is improved.",
+	"w3": "</p>",
+    "w4": "",
+	"w5": "<p>Level: challenging</p>"
+	}
+var p7_href = "https://github.com/kamil1006/kamil1006.github.io";
+
+
+
+
+var p8_html_content = {
+    "w1": "<p>First project in android.</p>",
+    "w4": "<p>Own version of simple game Papier kamien nozyczki.</p>",
+	"w5": "<p>Level: Android and java. Made over year ago. now it is simple but earlier...</p>"
+	
+}
+
+var p8_href = "https://github.com/kamil1006/Papier-Kamien-Nozyczki";
+var p8_href2 = "";
+var p8_video = "https://youtu.be/Rc1X7VMYuS8";
+var p8_href3 = "";
+
+
+
+var p9_nazwa = "Kółko i krzyzyk reloaded";
+var p9_html_content = {
+    "w1": "<p>Second project in android.</p>",
+    "w4": "<p>Own version of simple game .</p>",
+	"w5": "<p>Level: Android and java. Made over year ago. now it is not so hard but has opponent AI .</p>"
+	
+}
+
+var p9_href = "https://github.com/kamil1006/Kolko_i_krzyzyk_reloaded";
+var p9_href2 = "";
+var p9_video = "https://youtu.be/3iQmJqHtPVw";
+var p9_href3 = "";
+
+
+var p10_nazwa = "Tajemniczy zamek";
+var p10_html_content = {
+    "w1": "<p>Last project in android.</p>",
+    "w4": "<p>Own version of simple game based on a Bajtek newspaper's game from 90' .</p>",
+	"w5": "<p>Level: Android and java. Simple.</p>"
+	
+}
+
+var p10_href = "https://github.com/kamil1006/Tajemniczy_Zamek";
+var p10_href2 = "";
+var p10_video = "https://youtu.be/vvfIfIpxxa4";
+var p10_href3 = "";
+
+
+var p11_nazwa = "0 - Google search";
+var p11_html_content = {
+    "w1": "<p>Design a front-end for Google Search, Google Image Search, and Google Advanced Search.</p>",
+    "w4": "<p>Own version of google search website made in html with css.</p>",
+	"w5": "<p>Level: Simple.</p>"
+	
+}
+
+var p11_href = "";
+var p11_href2 = "";
+var p11_video = "https://youtu.be/y8F5SDu7ktk";
+var p11_href3 = "https://cs50.harvard.edu/web/2020/projects/0/search/";
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+
+var p12_nazwa = "1 - Wikipedia";
+var p12_html_content = {
+    "w1": "<p>Design a Wikipedia-like online encyclopedia..</p>",
+    "w4": "<p>Own version of wikipedia like website made in html with css.</p>",
+	"w5": "<p>Level: Simple.</p>"
+	
+}
+
+var p12_href = "";
+var p12_href2 = "";
+var p12_video = "https://youtu.be/qoMnNFalprQ";
+var p12_href3 = "https://cs50.harvard.edu/web/2020/projects/1/wiki/";
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+var p13_nazwa = "2 - Commerce";
+var p13_html_content = {
+    "w1": "<p>Design an eBay-like e-commerce auction site that will allow users to post auction listings, place bids ",
+    "w4": "on listings, comment on those listings, and add listings to a watchlist.</p>",
+	"w3": "<p>Done in python, django, html, css, javascript</p>",
+	"w5": "<p>Level: challenging.</p>"
+	
+}
+
+var p13_href = "";
+var p13_href2 = "";
+var p13_video = "https://youtu.be/zhQYxupBL1w";
+var p13_href3 = "https://cs50.harvard.edu/web/2020/projects/2/commerce/";
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+var p14_nazwa = "3 - mail";
+var p14_html_content = {
+    "w1": "<p>a front-end for an email client that makes API calls to send and receive emails.</p> ",
+   
+	"w3": "<p>Done in python, django, html, css, javascript</p>",
+	"w5": "<p>Level: challenging.</p>"
+	
+}
+
+var p14_href = "";
+var p14_href2 = "";
+var p14_video = "https://youtu.be/muFOK637BQU";
+var p14_href3 = "https://cs50.harvard.edu/web/2020/projects/3/mail/";
+
+//-------------------------------------------------------------------------------------------------------------
+
+var p15_nazwa = "3a - mail - JAVA";
+var p15_html_content = {
+    "w1": "<p>a front-end for an email client that makes API calls to send and receive emails.</p> ",
+   
+	"w3": "<p>Done in Java, thymeleaf, html, css, javascript</p>",
+	"w5": "<p>Level: challenging.</p>"
+	
+}
+
+var p15_href = "https://github.com/kamil1006/email_in_java";
+var p15_href2 = "";
+var p15_video = "https://youtu.be/muFOK637BQU";
+var p15_href3 = "https://cs50.harvard.edu/web/2020/projects/3/mail/";
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+var p16_nazwa = "4 - network";
+var p16_html_content = {
+    "w1": "<p>a Twitter-like social network website for making posts and following users..</p> ",
+   
+	"w3": "<p>Done in python, django, html, css, javascript</p>",
+	"w5": "<p>Level: challenging.</p>"
+	
+}
+
+var p16_href = "";
+var p16_href2 = "";
+var p16_video = "https://youtu.be/VaaBN2I-Vjk";
+var p16_href3 = "https://cs50.harvard.edu/web/2020/projects/4/network/";
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+var p17_nazwa = "4 -Capstone-miniHR";
+var p17_html_content = {
+    "w1": "<p>Site to plan and manage employees leaves </p> ",
+   
+	"w3": "<p>Done in python, django, html, css, javascript, jquery, REACT</p>",
+	"w5": "<p>Level: Very challenging.</p>"
+	
+}
+
+var p17_href = "";
+var p17_href2 = "";
+var p17_video = "";
+var p17_href3 = "";
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+
+
+var projekty = [
+ "Code sharing platform",
+ "Car sharing",
+ "Battleship",
+ "Web quiz engine",
+ "Text Editor",
+ "Json database",
+ "Portfolio",
+ "Google Search"
+];
+
+var projektyPortal = [
+ "JetBrains",
+"JetBrains",
+"JetBrains",
+"JetBrains",
+"JetBrains",
+"JetBrains",
+"JetBrains",
+"Harvard"
+];
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+
+var projekty2 = [
+  p1_html_content,
+  p2_html_content,
+  p3_html_content,
+  p4_html_content,
+ p5_html_content,
+ p6_html_content,
+ p7_html_content,
+ p8_html_content
+];
+
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+var projekt0 = {
+	name : "Kółko i krzyżyk",
+	content : p0_html_content,
+	lacze : p0_href,
+	portal : "MSOffice",
+	obraz1 : p0_href2,
+	video1: p0_video,
+	opis2: p0_href3
+}
+	
+var projekt1 = {
+	name : "Code sharing platform",
+	content : p1_html_content,
+	lacze : p1_href,
+	portal : "JetBrains"
+	
+}
+
+var projekt2 = {
+	name : "Car sharing",
+	content : p2_html_content,
+	lacze : p2_href,
+	portal : "JetBrains"
+	
+}
+
+var projekt3 = {
+	name : "Battleship",
+	content : p3_html_content,
+	lacze : p3_href,
+	portal : "JetBrains"
+	
+}
+
+var projekt4 = {
+	name : "Web quiz engine",
+	content : p4_html_content,
+	lacze : p4_href,
+	portal : "JetBrains"
+	
+}
+
+var projekt5 = {
+	name : "Text Editor",
+	content : p5_html_content,
+	lacze : p5_href,
+	portal : "JetBrains",
+	obraz1: p5_href2
+	
+}
+
+var projekt6 = {
+	name : "Json database",
+	content : p6_html_content,
+	lacze : p6_href,
+	portal : "JetBrains"
+	
+}
+
+
+var projekt7 = {
+	name : "Portfolio",
+	content : p7_html_content,
+	lacze : p7_href,
+	portal : "JetBrains"
+	
+}
+
+var projekt8 = {
+	name : "papier kamien",
+	content : p8_html_content,
+	lacze : p8_href,
+	portal : "Android",
+	video1: p8_video,
+	opis2: p8_href3,
+	obraz1 : p8_href2,
+	
+}
+
+
+var projekt9 = {
+	name : p9_nazwa,
+	content : p9_html_content,
+	lacze : p9_href,
+	portal : "Android",
+	video1: p9_video,
+	opis2: p9_href3,
+	obraz1 : p9_href2,
+	
+}
+
+var projekt10 = {
+	name : p10_nazwa,
+	content : p10_html_content,
+	lacze : p10_href,
+	portal : "Android",
+	video1: p10_video,
+	opis2: p10_href3,
+	obraz1 : p10_href2,
+	
+}
+
+var projekt11 = {
+	name : p11_nazwa,
+	content : p11_html_content,
+	lacze : p11_href,
+	portal : "Harvard",
+	video1: p11_video,
+	opis2: p11_href3,
+	obraz1 : p11_href2,
+	
+}
+
+var projekt12 = {
+	name : p12_nazwa,
+	content : p12_html_content,
+	lacze : p12_href,
+	portal : "Harvard",
+	video1: p12_video,
+	opis2: p12_href3,
+	obraz1 : p12_href2,
+	
+}
+
+
+var projekt13 = {
+	name : p13_nazwa,
+	content : p13_html_content,
+	lacze : p13_href,
+	portal : "Harvard",
+	video1: p13_video,
+	opis2: p13_href3,
+	obraz1 : p13_href2,
+	
+}
+
+
+var projekt14 = {
+	name : p14_nazwa,
+	content : p14_html_content,
+	lacze : p14_href,
+	portal : "Harvard",
+	video1: p14_video,
+	opis2: p14_href3,
+	obraz1 : p14_href2,
+	
+}
+
+var projekt15 = {
+	name : p15_nazwa,
+	content : p15_html_content,
+	lacze : p15_href,
+	portal : "Harvard but Java",
+	video1: p15_video,
+	opis2: p15_href3,
+	obraz1 : p15_href2,
+	
+}
+
+
+var projekt16 = {
+	name : p16_nazwa,
+	content : p16_html_content,
+	lacze : p16_href,
+	portal : "Harvard",
+	video1: p16_video,
+	opis2: p16_href3,
+	obraz1 : p16_href2,
+	
+}
+
+var projekt17 = {
+	name : p17_nazwa,
+	content : p17_html_content,
+	lacze : p17_href,
+	portal : "Harvard",
+	video1: p17_video,
+	opis2: p17_href3,
+	obraz1 : p17_href2,
+	
+}
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+var projekty3 = [
+	projekt0,
+	projekt8,
+	projekt9,
+	projekt10,
+	projekt5,
+	projekt1,
+	projekt2,
+	projekt3,
+	projekt4,
+	
+	projekt6,
+	projekt7,
+	projekt11,
+	projekt12,
+	projekt13,
+	projekt14,
+	projekt15,
+	projekt16,
+	projekt17
+	
+];
+
+
+
+
+
+   
+
+
+
+
---
+++ scripts/script.js
@@ -0,0 +1,275 @@
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+    //------------------------------------------------------
+
+	generujProjekty(projekty,projekty3);
+  //  generujAkcjePrzyciskow();
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+//  https://softauthor.com/javascript-for-loop-click-event-issues-solutions/
+	for(var i in projekty3) {
+		let ww=i*1+1;
+		var button = document.getElementById("button_"+ww);
+		//button.innerText = i;
+		(function(index){
+		  button.addEventListener("click", function() {
+
+
+			displayAchievments2(projekty3[index]);
+		  })
+		})(i)
+	  
+	  
+	  }
+ 
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+   
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        restartContainer();
+        modal.style.display = "none";
+
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            restartContainer();
+            modal.style.display = "none";
+            //restartContainer();
+        }
+    }
+
+    //console.log("javascript!");
+
+    //------------------------------------------------------
+});
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+function displayAchievments(a,b,b2,c){
+
+
+
+    const e1 = document.createElement('div');
+    e1.id="post_content";
+    e1.className = "container border justify-content-between";
+    e1.setAttribute("style","background-color: white;");
+
+    const e_a = document.createElement('p');
+    e_a.innerHTML=a;
+    const e_b = document.createElement('p');
+    e_b.innerHTML=b;
+    const e_b2 = document.createElement('a');
+    e_b2.innerHTML=b2;
+    e_b2.setAttribute("href",b2);
+    const e_c = document.createElement('p');
+    e_c.innerHTML=c;
+
+    e1.append(e_a);
+    e1.append(e_b);
+    e1.append(e_b2);
+    e1.append(e_c);
+    var el2 = document.getElementById('modal-body-real');
+    el2.append(e1);
+
+
+}
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+function displayAchievments2(w){
+
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+
+    console.log("jestem "+w.name);
+   var  html_content = w.content;
+   var p_href =  w.lacze;
+   var p_href2 =  w.obraz1;
+    var p_href3 =  w.opis2;
+    var p_video =  w.video1;
+   
+    const e1 = document.createElement('div');
+    e1.id="post_content";
+    e1.className = "container border justify-content-between";
+    e1.setAttribute("style","background-color: white;");
+
+		for(var w in html_content) {
+			
+				
+				e1.innerHTML += html_content[w];
+		}		
+    
+	
+	//console.log("hrefffffffffff="+p_href[0]);
+	//--------------------------
+	if(p_href != null && typeof(p_href[0]) != "undefined" ){
+		
+		const e_b2 = document.createElement('a');
+		e_b2.innerHTML="Github source code";
+		e_b2.setAttribute("href",p_href);
+		
+		e1.append(e_b2);
+		
+			const pp = document.createElement('p');
+			e1.append(pp);
+	}
+	//--------------------------
+	if(p_video != null && typeof(p_video[0]) != "undefined" ){
+		
+		const e_b2 = document.createElement('a');
+		e_b2.innerHTML="Youtube presentation";
+		e_b2.setAttribute("href",p_video);
+		
+		e1.append(e_b2);
+		
+			const pp = document.createElement('p');
+			e1.append(pp);
+	}
+	//--------------------------
+	if(p_href3 != null && typeof(p_href3[0]) != "undefined" ){
+		
+		const e_b2 = document.createElement('a');
+		e_b2.innerHTML="Additional detailed specification";
+		e_b2.setAttribute("href",p_href3);
+		
+		e1.append(e_b2);
+		
+			const pp = document.createElement('p');
+			e1.append(pp);
+	}
+	//--------------------------
+	
+	
+	if(p_href2 != null ){
+			const img1 = document.createElement('img');
+			//img1.innerHTML="Github link";
+			img1.className ="portfelObraz";
+			img1.setAttribute("src",p_href2);
+			e1.append(img1);
+		
+	}
+	
+	
+	
+	
+	
+    var el2 = document.getElementById('modal-body-real');
+    el2.append(e1);
+	
+    
+	
+
+
+}
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+function restartContainer(){
+    var el = document.getElementById('modal-body-real');
+    if(el === null){
+        console.log("nie usuniete");
+    }
+
+    else{
+        el.remove(); // Removes the div with the 'post_container' id
+        console.log("usuniete");
+    }
+
+    const e0 = document.createElement('div');
+    e0.id="modal-body-real";
+
+    var el2 = document.getElementById('modal-body');
+    el2.append(e0);
+    // document.querySelector('#posts-view').style.display = 'block';
+
+
+
+
+}
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+function generujProjekty(projekty,projekty3){
+	
+	
+	 var el2 = document.getElementById('wykaz');
+ 
+     console.log(projekty3);
+	
+	var i = 1;
+	for(var w in projekty3) {
+				
+		const e1 = document.createElement('div');
+		 e1.className = "project_item";
+		 //switch(projektyPortal[w]){
+            switch(projekty3[w].portal){    
+			case "Harvard":
+				  e1.setAttribute("style","background-color: #ffe6e6;");
+
+				break;
+			case "MSOffice":
+					 e1.setAttribute("style","background-color: lightgreen;");
+				break;
+				
+			case "Android":
+					 e1.setAttribute("style","background-color: #ffff99;");
+				break;	
+			 
+		 }
+		 
+		const e11 = document.createElement('p');
+		 e11.className = "project_name";
+		 e11.innerHTML = projekty3[w].name;
+		 const e12 = document.createElement('button');
+		 e12.className = "open-window";
+		 e12.id="button_"+ i;
+		 e12.innerHTML="Details";
+		 
+		
+		
+	
+	
+		 
+		const e13 = document.createElement('hr');
+		const e14 = document.createElement('p');	
+		 e14.innerHTML ="made with "+projekty3[w].portal;
+		
+		e1.append(e11);
+		e1.append(e12);
+		e1.append(e13);
+		e1.append(e14);
+		el2.append(e1);
+		
+		i++;
+		}		
+	
+}
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
